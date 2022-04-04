c404f3715a2ab98bda3f887441b01f1c38829546
RS201918703 <rachelsohzc@gmail.com>
2022-04-04 15:49:24 +0100

    Update Draft codes.py

--- Draft codes.py
+++ Draft codes.py
@@ -1,8 +1,20 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Apr  4 15:22:17 2022
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-@author: User
-"""
+url = 'FB.csv'
+meta = pd.read_csv(url)
+nvidia = meta.dropna(how='any',axis=0) #If there is missing data
+meta['Date'].apply(pd.to_datetime) #For ease of adjusting dates later on
+len(meta.index)
 
-print('test')
+meta = meta.sort_values('Date')
+meta.head()
+
+plt.figure(figsize = (20,9))
+plt.plot(meta.Date, meta.Close)
+plt.xticks(range(0,meta.shape[0],100),meta['Date'].loc[::100],rotation=45)
+plt.xlabel('Date',fontsize=12)
+plt.ylabel('Mid price',fontsize=12)
+plt.title('Meta stock price changes over time',fontsize=20)
+plt.show()
